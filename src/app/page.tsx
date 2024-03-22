2fb05c70ea1878ea4b42892fe09509d3d7d67e21
Jowin Rojas <jowinx-001@hotmail.com>
2024-03-22 07:00:14 -0500

    zonas del home
    se pueba codigo de alineacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex justify-end">
+      <h1 className="ml-5 mr-5">Carousel</h1>
+      <h1 className="ml-5 mr-5">Servicios</h1>
+      <h1 className="ml-5 mr-5">precios</h1>
+      <h1 className="ml-5 mr-5">Fotter</h1>
     </div>
   );
 }
