a790fc87461a6c7402930b8ffb02b3f1f4d8f64d
Jowin Rojas <jowinx-001@hotmail.com>
2024-03-21 23:53:59 -0500

    framer motion
    instalacion
    ajuste
    consulta
    aplicacion
    prueba

--- src/app/reservas/page.tsx
+++ src/app/reservas/page.tsx
@@ -1,7 +1,22 @@
+'use client'
+
+import React from "react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import "./styles.css";
+
 export default function ReservasPage() {
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleSwitch = () => setIsOn(!isOn);
   return (
-    <div>
-      <h1>Reservas</h1>
+    <div className="switch" data-isOn={isOn} onClick={toggleSwitch}>
+      <motion.div className="handle" layout transition={spring} />
     </div>
   )
-}
+} 
+  const spring = {
+    type: "spring",
+    stiffness: 700, /*velocidad*/
+    damping: 40 /* rebote */
+  };
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 
 
 const links = [
@@ -23,38 +24,40 @@ const links = [
 export default function NavBar() {
 
   const pathname = usePathname()
+  const MotionLink = motion(Link);
+  const MotionImage = motion(Image);
 
   return (
-    <div>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <motion.div>
+      <motion.nav className="bg-white border-gray-200 dark:bg-gray-900">
 
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <motion.div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
-          <span className="flex items-center space-x-3 rtl:space-x-reverse">
-            <Image src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" width={32} height={33} style={{ width: 'auto', height: 'auto' }}></Image>
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Soluman</span>
-          </span>
+          <motion.span className="flex items-center space-x-3 rtl:space-x-reverse">
+            <MotionImage src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" width={32} height={33} style={{ width: 'auto', height: 'auto' }}></MotionImage>
+            <motion.span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Soluman</motion.span>
+          </motion.span>
 
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <motion.div className="hidden w-full md:block md:w-auto" id="navbar-default">
 
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <motion.ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               {links.map((link) => {
                 return (
-                  <li key={link.name}>
-                    <Link
+                  <motion.li key={link.name}>
+                    <MotionLink
                       key={link.name}
                       href={link.href}
                       className={`block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>
-                      <p className="hidden md:block">{link.name}</p>
-                    </Link>
-                  </li>
+                      <motion.p className="hidden md:block">{link.name}</motion.p>
+                    </MotionLink>
+                  </motion.li>
                 )
               })}
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+            </motion.ul>
+          </motion.div>
+        </motion.div>
+      </motion.nav>
+    </motion.div>
   );
 }
 
